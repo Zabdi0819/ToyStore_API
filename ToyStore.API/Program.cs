770bdc3a0bc0035ea57ed60186b5de34ad66e224
ZabR19 <zabdi.ramirez@lionintel.com>
2025-08-11 23:06:52 -0600

    Services update

--- ToyStore.API/Program.cs
+++ ToyStore.API/Program.cs
@@ -22,7 +22,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000") // React
+        policy.WithOrigins("http://localhost:5173",
+                "https://localhost:44370")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
